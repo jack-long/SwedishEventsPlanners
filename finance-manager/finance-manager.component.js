8769985111c4ba324a79f271326e8fae6e39efbf
rigvedpatki <rigved.patki@gmail.com>
2016-10-18 11:54:06 +0200

    modified components

---
+++ finance-manager/finance-manager.component.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.
+  module('financeManager').
+  component('financeManager', {
+    templateUrl: 'finance-manager/finance-manager.template.html',
+    controller: ['$http', function View1Controller($http) {
+
+    }]
+  });
---
+++ hr-manager/hr-manager.component.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.
+  module('hrManager').
+  component('hrManager', {
+    templateUrl: 'hr-manager/hr-manager.template.html',
+    controller: ['$http', function View1Controller($http) {
+
+    }]
+  });
