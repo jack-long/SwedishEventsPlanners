13209af0df03be35c0a23f20eab73aad70b786a0
rigvedpatki <rigved.patki@gmail.com>
2016-10-20 17:27:52 +0200

    modifications to hr

--- hr-manager/hr-manager.component.js
+++ hr-manager/hr-manager.component.js
@@ -4,7 +4,9 @@ angular.
   module('hrManager').
   component('hrManager', {
     templateUrl: 'hr-manager/hr-manager.template.html',
-    controller: ['$http', function View1Controller($http) {
+    controller: ['DB', function View1Controller(DB) {
+      var search_emplyee = DB.employee;
+      var search_recruitment_request=DB.recruitment_request;
 
     }]
   });
