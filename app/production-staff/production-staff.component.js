158a3f128672211ea0d9b742dc951e9b6d90803b
rigvedpatki <rigved.patki@gmail.com>
2016-10-23 03:11:18 +0200

    Modified production staff

--- app/production-staff/production-staff.component.js
+++ app/production-staff/production-staff.component.js
@@ -4,7 +4,21 @@ angular.
   module('productionStaff').
   component('productionStaff', {
     templateUrl: 'production-staff/production-staff.template.html',
-    controller: ['$http', function View1Controller($http) {
+    controller: ['$scope','DB', function View1Controller($scope, DB) {
 
+      //display all tasks
+      var assignedTasks = DB.task;
+      $scope.displayedTasks = assignedTasks;
+
+      //display task Details
+      $scope.displayTaskDetails = function(task){
+        $scope.task = task ;
+      };
+
+      $scope.updateTask = function() {
+        alert("New comment added to the task: \n" + $scope.newComment);
+        // todo : create array of comments and add the new comment to it
+      };
+      
     }]
   });
