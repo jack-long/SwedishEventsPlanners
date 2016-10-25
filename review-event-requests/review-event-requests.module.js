fcc1c5150edc75d72e720911d4a6b34fdea7fad6
rigvedpatki <rigved.patki@gmail.com>
2016-10-25 23:51:31 +0200

    Deleted unwanted files

--- review-event-requests/review-event-requests.module.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// modification to one event request:
-// reviewer: senior customer service -> financial service -> admin
-angular.module('reviewEventRequests', [
-  'ngRoute'
-]);
--- view1/view1.module.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Define the `phoneDetail` module
-angular.module('view1', [
-  'ngRoute'
-]);
